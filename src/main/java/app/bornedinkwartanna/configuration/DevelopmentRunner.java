cce68763dbca01a10d4d2fc2c8f822e420451eaa
Tobiasz Kubiak <tobiaszkubiak31@gmail.com>
2020-06-20 09:28:36 +0200

    fix

--- src/main/java/app/bornedinkwartanna/configuration/DevelopmentRunner.java
+++ src/main/java/app/bornedinkwartanna/configuration/DevelopmentRunner.java
@@ -7,29 +7,36 @@ import app.bornedinkwartanna.data.StateRepository;
 import app.bornedinkwartanna.domain.Product;
 
 import app.bornedinkwartanna.domain.State;
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+@Slf4j
 @Configuration
 public class DevelopmentRunner {
 
-    public static final String CSV_LOCATION_PATH = "src\\main\\java\\app\\bornedinkwartanna\\configuration\\csv";
+    public static final String CSV_LOCATION_PATH = "src/main/java/app/bornedinkwartanna/configuration/csv";
 
     @Bean
     public CommandLineRunner dataLoader(
         ProductRepository productRepository, StateRepository stateRepository) { // user repo for ease of testing with a built-in user
         return args -> {
+
+            System.out.println( new File("").getAbsolutePath());
             ProductCsvReader productCsvReader = new ProductCsvReader();
-            List<Product> products = productCsvReader.read(CSV_LOCATION_PATH + "\\products.csv");
+            List<Product> products = productCsvReader.read(CSV_LOCATION_PATH + "/products.csv");
             for (int i = 0; i < products.size(); i++) {
                 products.get(i).setSrc_image(new String(Files.readAllBytes(Paths.get("src/main/resources/images/img" + i + ".txt"))));
                 productRepository.save(products.get(i));
             }
+
 
             StateCsvReader stateCsvReader = new StateCsvReader();
             List<State> states = stateCsvReader.read(CSV_LOCATION_PATH + "\\states.csv");
